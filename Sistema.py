734ee4d2c930698e99c9d70db5efe06a8d59fb56
soyeldono <mosheh315@gmail.com>
2019-08-16 11:40:43 -0500

    cambie el sistema.py

--- Sistema.py
+++ Sistema.py
@@ -12,7 +12,9 @@ def function():
         L1.append(m.pow(x1,2)+1)
         L2.append(m.pow(x2,2)+1)
         L3.append(m.pow(x3,2)+1)
-        
+        x1=m.pow(x1,2)+1
+        x2=m.pow(x2,2)+1
+        x3=m.pow(x2,2)+1
     return L1,L2,L3
 
 #print(function())
