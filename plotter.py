c51066b3cad2712907e521d7e87ea182f9a5aa25
GalvanLuis <gruisu93@gmail.com>
2019-08-30 23:24:19 -0500

    Plotter updated: depends of main, calculate.py files

--- plotter.py
+++ plotter.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 from calculate import ReadEquation as RE
 import matplotlib.pyplot as plt
 
 
-def plotter(Yx, equation):
-    fig, ax = plt.subplots()
-    for i in list(Yx.keys()):
+def plotter(Yx, equation): #Crea una funcion para el plotter donde llevara los parametros obligatorios Xy que es el diccionario de los valores del eje Y a graficar y equation que es la ecuacion ingresada y que sera agregada como titulo
+    fig, ax = plt.subplots() #Asigna la variable fig y ax a los subplot para graficar los valores de la Y, aun no se usa fig
+    for i in list(Yx.keys()): #ciclo que recorre todas las llaves de los diccionarios
         #plt.plot(list(range(1,len(Yx[i])+1)), Yx[i])
-        ax.plot(Yx[i],label=equation)
-        plt.yscale('log')
-        plt.grid(True)
-        plt.xlabel('Y axis')
-        plt.ylabel('Num of iterations')
+        ax.plot(Yx[i],label=equation) # hace las graficas para todos los valores que contienen las llaves, o sea los valores de Y
+        plt.yscale('log') #Escala las graficas 
+        plt.grid(True) #Hace una cuadricula en la figura
+        plt.xlabel('Y axis') #Etiqueta del eje X, aun no esta definida
+        plt.ylabel('Num of iterations') #Etiqueta del eje Y, aun no esta definida
         #plt.setp('linewidth', 2.0)
         #plt.title('Aqui va equation','equation')
    
-        plt.show()
+        plt.show() #muestra la grafica
 
 
   
     
-#observaciones o funciones analiticas
+#observaciones o funciones analiticas: ignorar esto.
